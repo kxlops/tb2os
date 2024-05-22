6ec663c50ff06200d54e483ab61ba507e2b0d9c3
Chris Konkel <4850145+ckonkel@users.noreply.github.com>
2024-05-21 23:42:47 -0500

    add flat grid overlay

--- tb2-rotate-animation-esp32.ino
+++ tb2-rotate-animation-esp32.ino
@@ -31,15 +31,31 @@ void displayReset(void) {
   delay(10);
 }
 
-void displayFrame(const unsigned char* frame) {
+void drawGrid() {
+  int gridSize = 9;
+  int spacingX = 128 / gridSize;
+  int spacingY = 64 / gridSize;
+
+  for (int x = 0; x <= 128; x += spacingX) {
+    display.drawLine(x, 0, x, 64);
+  }
+
+  for (int y = 0; y <= 64; y += spacingY) {
+    display.drawLine(0, y, 128, y);
+  }
+}
+
+void displayFrame(const unsigned char* frame, int frameNumber) {
   display.clear();
   display.drawXbm(0, 0, 128, 64, frame);
+  drawGrid();
+  display.drawString(0, 54, "Frame: " + String(frameNumber + 1));
   display.display();  // Ensure buffer is sent to display
-  Serial.println("Frame displayed");
+  Serial.println("Frame displayed: " + String(frameNumber + 1));
 }
 
 void showLoadingScreen() {
-  displayFrame(frames[0]);  // Display the first frame
+  displayFrame(frames[0], 0);  // Display the first frame
   delay(5000);  // Display for 5 seconds
 }
 
@@ -88,7 +104,7 @@ void loop() {
     }
     Serial.println();
 
-    displayFrame(frames[i]);
+    displayFrame(frames[i], i);
     delay(FRAME_DELAY);
   }
 }
